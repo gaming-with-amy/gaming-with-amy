1f9704df5a90f2de966a0427a1ab8bea4c8c183e
gaming-with-amy <amysidwell481@gmail.com>
2025-10-05 13:18:34 -0500

    added webpack setup and dev script...i have no clue what im doing lmaooo

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 5173,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './templates/index.html', 
+      filename: 'index.html',
+    }),
+  ],
+};
